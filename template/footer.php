05b4833b7c962433018d13ac6c87df02c589299b
Javier Rios <44488973+javierriosr@users.noreply.github.com>
2022-09-19 00:53:48 -0500

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
 <footer class="container-fluid bg-dark fixed-bottom">
         <div class="row">
             <div class="col-md text-light text-center py-3">
-                Desarrollado por Roger Gómez
+                Desarrollado por Javier Alexander Rios Rodriguez
             </div>
         </div>
     </footer>
